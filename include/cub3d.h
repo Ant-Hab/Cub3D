07cc6d1580b664ea8dad388766335d77c6e0857c
Jaekwang Lee <tytut233@gmail.com>
2026-02-17 15:59:49 +0200

    add:data_structure

--- include/cub3d.h
+++ include/cub3d.h
@@ -59,3 +59,82 @@ void	error_exit(t_game *game, char *msg);
 void	free_game(t_game *game);
 
 #endif
+// refer Data Structure
+
+// typedef struct s_player
+// {
+// 	double	x;
+// 	double	y;
+// 	char	view;
+// 	double	dir_x;
+// 	double	dir_y;
+// 	double	plane_x;
+// 	double	plane_y;
+// }	t_player;
+
+// typedef struct s_map
+// {
+// 	char			**chart;
+// 	int				max_x;
+// 	int				max_y;
+// 	int				start_line;
+// 	t_player		*player;
+// 	uint32_t		floor_color;
+// 	uint32_t		ceil_color;
+// 	char			*north;
+// 	char			*south;
+// 	char			*west;
+// 	char			*east;
+// 	mlx_texture_t	*north_tex;
+// 	mlx_texture_t	*south_tex;
+// 	mlx_texture_t	*west_tex;
+// 	mlx_texture_t	*east_tex;
+// }	t_map;
+
+// typedef struct s_game
+// {
+// 	t_map		*map;
+// 	t_player	*player;
+// 	mlx_t		*mlx;
+// 	mlx_image_t	*img;
+// 	int			width;
+// 	int			height;
+// 	bool		forward;
+// 	bool		back;
+// 	bool		left;
+// 	bool		right;
+// 	bool		rotate_left;
+// 	bool		rotate_right;
+// }	t_game;
+
+// typedef struct s_ray_dir
+// {
+// 	double	camera_x;
+// 	double	dir_x;
+// 	double	dir_y;
+// 	int		map_x;
+// 	int		map_y;
+// 	double	side_dist_x;
+// 	double	side_dist_y;
+// 	double	delta_dist_x;
+// 	double	delta_dist_y;
+// 	int		step_x;
+// 	int		step_y;
+// 	int		hit_wall;
+// 	int		side;
+// }	t_ray_dir;
+
+// typedef struct s_ray_wall
+// {
+// 	double	perp_dist;
+// 	int		line_height;
+// 	int		start_draw;
+// 	int		end_draw;
+// 	int		texture;
+// }	t_ray_wall;
+
+// typedef struct s_ray
+// {
+// 	t_ray_dir	dir;
+// 	t_ray_wall	wall;
+// }	t_ray;
