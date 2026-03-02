a25b27ca2d6545b4915fe4dca5b8b5ee4d23d6ad
Jaekwang Lee <tytut233@gmail.com>
2026-03-02 16:40:41 +0200

    move:clean

--- src/excution/utilities.c
+++ src/excution/utilities.c
@@ -6,48 +6,12 @@
 /*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 11:31:48 by jaeklee           #+#    #+#             */
-/*   Updated: 2026/03/02 14:20:32 by jaeklee          ###   ########.fr       */
+/*   Updated: 2026/03/02 16:39:26 by jaeklee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void	free_game(t_game *game)
-{
-	if (!game)
-		return;
-
-	// 텍스처 먼저 해제 (mlx 필요)
-	free_textures(game);
-
-	// 이미지 해제
-	if (game->img)
-		mlx_delete_image(game->mlx, game->img);
-
-	// map 해제
-	if (game->map)
-	{
-		if (game->map->grid)
-			free_tab(game->map->grid);
-		free(game->map);
-	}
-
-	// MLX 종료 (제일 마지막)
-	if (game->mlx)
-		mlx_terminate(game->mlx);
-}
-
-void	error_exit(t_game *game, char *msg)
-{
-	if (msg)
-		fprintf(stderr, "Error: %s\n", msg);
-
-	// 게임 관련 메모리 해제
-	if (game)
-		free_game(game);
-
-	exit(EXIT_FAILURE);
-}
 
 void key_press(mlx_key_data_t keydata, void *param)
 {
--- src/utils/utils.c
+++ src/utils/utils.c
@@ -6,27 +6,52 @@
 /*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/01/27 14:35:45 by achowdhu          #+#    #+#             */
-/*   Updated: 2026/03/02 14:48:33 by jaeklee          ###   ########.fr       */
+/*   Updated: 2026/03/02 16:40:21 by jaeklee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
 /* Prints error message, frees allocated game memory, and exits program */
-// void	error_exit(t_game *game, char *msg)
-// {
-// 	write(2, "Error\n", 6);
-// 	if (msg)
-// 	{
-// 		write(2, msg, ft_strlen(msg));
-// 		write(2, "\n", 1);
-// 	}
-// 	else
-// 		perror("System Error");
-// 	if (game)
-// 		free_game(game);
-// 	exit(1);
-// }
+
+void	free_game(t_game *game)
+{
+	if (!game)
+		return;
+
+	// 텍스처 먼저 해제 (mlx 필요)
+	free_textures(game);
+
+	// 이미지 해제
+	if (game->img)
+		mlx_delete_image(game->mlx, game->img);
+
+	// map 해제
+	if (game->map)
+	{
+		if (game->map->grid)
+			free_tab(game->map->grid);
+		free(game->map);
+	}
+
+	// MLX 종료 (제일 마지막)
+	if (game->mlx)
+		mlx_terminate(game->mlx);
+}
+void	error_exit(t_game *game, char *msg)
+{
+	write(2, "Error\n", 6);
+	if (msg)
+	{
+		write(2, msg, ft_strlen(msg));
+		write(2, "\n", 1);
+	}
+	else
+		perror("System Error");
+	if (game)
+		free_game(game);
+	exit(1);
+}
 
 /* Frees a NULL-terminated array of strings */
 void	free_tab(char **tab)
