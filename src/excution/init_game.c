19c8c735508f7c5fdbb0e67b457b06327e4a69d4
Jaekwang Lee <tytut233@gmail.com>
2026-03-05 15:09:30 +0200

    fix:norminette

--- src/excution/init_game.c
+++ src/excution/init_game.c
@@ -6,7 +6,7 @@
 /*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/23 11:09:19 by jaeklee           #+#    #+#             */
-/*   Updated: 2026/03/03 10:41:58 by jaeklee          ###   ########.fr       */
+/*   Updated: 2026/03/05 14:56:36 by jaeklee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,12 @@ void	init_graphics(t_game *game)
 {
 	game->width = WIDTH;
 	game->height = HEIGHT;
-
-	// MLX42 초기화
 	game->mlx = mlx_init(game->width, game->height, "cub3D", true);
 	if (!game->mlx)
 		error_exit(game, "MLX inifailed");
-
-	// 이미지 생성
 	game->img = mlx_new_image(game->mlx, game->width, game->height);
 	if (!game->img)
 		error_exit(game, "Image creation failed");
-
-	// 이미지 윈도우에 붙이기
 	if (mlx_image_to_window(game->mlx, game->img, 0, 0) < 0)
 		error_exit(game, "Image display failed");
 }
@@ -36,18 +30,13 @@ void	init_graphics(t_game *game)
 void	init_game_state(t_game *game, t_map *map)
 {
 	game->map = map;
-
-	// player 생성
 	game->player = malloc(sizeof(t_player));
 	if (!game->player)
 		error_exit(game, "Player malloc failed");
-
-	// 위치 설정 (중앙 정렬)
 	game->player->x = map->p_x + 0.5f;
 	game->player->y = map->p_y + 0.5f;
 	game->player->move_speed = 0.1f;
 	game->player->rot_speed = 0.05f;
-
 	init_player_direction(game->player, map->start_dir);
 	game->forward = false;
 	game->back = false;
@@ -60,16 +49,46 @@ void	init_game_state(t_game *game, t_map *map)
 void	init_player_direction(t_player *p, char dir)
 {
 	if (dir == 'N')
-		(p->dir_x = 0, p->dir_y = -1);
+	{
+		p->dir_x = 0;
+		p->dir_y = -1;
+	}
 	else if (dir == 'S')
-		(p->dir_x = 0, p->dir_y = 1);
+	{
+		p->dir_x = 0;
+		p->dir_y = 1;
+	}
 	else if (dir == 'E')
-		(p->dir_x = 1, p->dir_y = 0);
+	{
+		p->dir_x = 1;
+		p->dir_y = 0;
+	}
 	else if (dir == 'W')
-		(p->dir_x = -1, p->dir_y = 0);
-
-	// 카메라 평면은 방향 벡터의 수직 벡터
+	{
+		p->dir_x = -1;
+		p->dir_y = 0;
+	}
 	p->plane_x = -p->dir_y * 0.66f;
 	p->plane_y = p->dir_x * 0.66f;
 }
 
+void	init_ray(t_game *game, t_ray *ray, int i)
+{
+	double	camera_x;
+
+	camera_x = 2.0 * i / (double)game->width - 1.0;
+	ray->dir_x = game->player->dir_x + game->player->plane_x * camera_x;
+	ray->dir_y = game->player->dir_y + game->player->plane_y * camera_x;
+	ray->map_x = (int)game->player->x;
+	ray->map_y = (int)game->player->y;
+	if (ray->dir_x == 0)
+		ray->del_dist_x = 1e30;
+	else
+		ray->del_dist_x = fabs(1 / ray->dir_x);
+	if (ray->dir_y == 0)
+		ray->del_dist_y = 1e30;
+	else
+		ray->del_dist_y = fabs(1 / ray->dir_y);
+	ray->hit_wall = 0;
+	ray->hit_axis = 0;
+}
--- src/excution/textures.c
+++ src/excution/textures.c
@@ -6,119 +6,94 @@
 /*   By: jaeklee <jaeklee@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/02/24 10:49:52 by jaeklee           #+#    #+#             */
-/*   Updated: 2026/03/05 13:58:26 by jaeklee          ###   ########.fr       */
+/*   Updated: 2026/03/05 15:08:28 by jaeklee          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void load_textures(t_game *game)
-{
-    if (!game->texture)
-        return;
-
-    game->texture->no_tex = mlx_load_png(game->texture->no);
-    if (!game->texture->no_tex)
-        error_exit(game, "Failed to load north texture");
-
-    game->texture->so_tex = mlx_load_png(game->texture->so);
-    if (!game->texture->so_tex)
-        error_exit(game, "Failed to load north texture");
-
-    game->texture->we_tex = mlx_load_png(game->texture->we);
-    if (!game->texture->we_tex)
-        error_exit(game, "Failed to load north texture");
-
-    game->texture->ea_tex = mlx_load_png(game->texture->ea);
-    if (!game->texture->ea_tex)
-        error_exit(game, "Failed to load north texture");
-}
-
-void	free_textures(t_game *game)
+void	load_textures(t_game *game)
 {
 	if (!game->texture)
-		return;
-
-	if (game->texture->no_tex)
-		mlx_delete_texture(game->texture->no_tex);
-	if (game->texture->so_tex)
-		mlx_delete_texture(game->texture->so_tex);
-	if (game->texture->we_tex)
-		mlx_delete_texture(game->texture->we_tex);
-	if (game->texture->ea_tex)
-		mlx_delete_texture(game->texture->ea_tex);
-	if (game->texture->no)
-		free(game->texture->no);
-	if (game->texture->so)
-		free(game->texture->so);
-	if (game->texture->we)
-		free(game->texture->we);
-	if (game->texture->ea)
-		free(game->texture->ea);
-
-	free(game->texture);
-	game->texture = NULL;
+		return ;
+	game->texture->no_tex = mlx_load_png(game->texture->no);
+	if (!game->texture->no_tex)
+		error_exit(game, "Failed to load north texture");
+	game->texture->so_tex = mlx_load_png(game->texture->so);
+	if (!game->texture->so_tex)
+		error_exit(game, "Failed to load north texture");
+	game->texture->we_tex = mlx_load_png(game->texture->we);
+	if (!game->texture->we_tex)
+		error_exit(game, "Failed to load north texture");
+	game->texture->ea_tex = mlx_load_png(game->texture->ea);
+	if (!game->texture->ea_tex)
+		error_exit(game, "Failed to load north texture");
 }
 
-/* 레이가 충돌한 벽에 맞는 텍스처 선택 */
-mlx_texture_t *texture_selection(t_ray *ray, t_game *game)
+mlx_texture_t	*texture_selection(t_ray *ray, t_game *game)
 {
-	if (ray->hit_axis == 0) // 세로벽
+	if (ray->hit_axis == 0)
 	{
 		if (ray->step_x > 0)
-			return game->texture->we_tex;
+			return (game->texture->we_tex);
 		else
-			return game->texture->ea_tex;
+			return (game->texture->ea_tex);
 	}
-	else // 가로벽
+	else
 	{
 		if (ray->step_y > 0)
-			return game->texture->no_tex;
+			return (game->texture->no_tex);
 		else
-			return game->texture->so_tex;
+			return (game->texture->so_tex);
 	}
 }
 
-/* 레이가 맞은 벽에서 텍스처의 컬럼 선택 */
-int get_texture_column(t_ray *ray, mlx_texture_t *tex, t_player *player)
+int	get_texture_column(t_ray *ray, mlx_texture_t *tex, t_player *player)
 {
-	double wall_x;
+	double	wall_x;
+	int		tex_x;
 
-	if (ray->hit_axis == 0) // 세로벽
+	if (ray->hit_axis == 0)
 		wall_x = player->y + ray->distance * ray->dir_y;
-	else // 가로벽
+	else
 		wall_x = player->x + ray->distance * ray->dir_x;
-
-	wall_x -= floor(wall_x); // 벽에서 x좌표만 가져오기
-	int tex_x = (int)(wall_x * (double)tex->width);
-
-	if ((ray->hit_axis == 0 && ray->dir_x > 0) ||
-		(ray->hit_axis == 1 && ray->dir_y < 0))
+	wall_x -= floor(wall_x);
+	tex_x = (int)(wall_x * (double)tex->width);
+	if ((ray->hit_axis == 0 && ray->dir_x > 0) || (ray->hit_axis == 1
+			&& ray->dir_y < 0))
 		tex_x = (int)tex->width - tex_x - 1;
-
-	return tex_x;
+	return (tex_x);
 }
 
 /* 스크린 y 위치에 따른 텍스처 row 계산 */
-int get_texture_row(int y, t_ray *ray, mlx_texture_t *tex)
+int	get_texture_row(int y, t_ray *ray, mlx_texture_t *tex)
 {
-	int wall_height = ray->draw_bottom - ray->draw_top + 1;
-	int tex_height = (int)tex->height;
-	int tex_y = (y - ray->draw_top) * tex_height / wall_height;
+	int	wall_height;
+	int	tex_height;
+	int	tex_y;
 
-	if (tex_y < 0) tex_y = 0;
-	if (tex_y >= tex_height) tex_y = tex_height - 1;
-
-	return tex_y;
+	wall_height = ray->draw_bottom - ray->draw_top + 1;
+	tex_height = (int)tex->height;
+	tex_y = (y - ray->draw_top) * tex_height / wall_height;
+	if (tex_y < 0)
+		tex_y = 0;
+	if (tex_y >= tex_height)
+		tex_y = tex_height - 1;
+	return (tex_y);
 }
 
-int get_texture_color(mlx_texture_t *tex, int x, int y)
+int	get_texture_color(mlx_texture_t *tex, int x, int y)
 {
-    int i = (y * tex->width + x) * 4;
-    unsigned char r = tex->pixels[i + 0];
-    unsigned char g = tex->pixels[i + 1];
-    unsigned char b = tex->pixels[i + 2];
-    unsigned char a = tex->pixels[i + 3]; // 텍스처에 alpha 없으면 255
+	int				i;
+	unsigned char	r;
+	unsigned char	g;
+	unsigned char	b;
+	unsigned char	a;
 
-    return (r << 24) | (g << 16) | (b << 8) | a; // MLX42 RGBA 포맷
+	i = (y * tex->width + x) * 4;
+	r = tex->pixels[i + 0];
+	g = tex->pixels[i + 1];
+	b = tex->pixels[i + 2];
+	a = tex->pixels[i + 3];
+	return ((r << 24) | (g << 16) | (b << 8) | a);
 }
